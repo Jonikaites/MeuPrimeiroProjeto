04b3d7fedd0583137d91987901b416c2d503dccb
Tecnico - TI <tecnico@epfserver.com.br>
2016-05-02 22:46:16 -0300

    inclusão do metodo somar

---
+++ src/meuprimeiroprojeto/Calculadora.java
@@ -0,0 +1,34 @@
+package meuprimeiroprojeto;
+
+public class Calculadora {
+	
+	private int valor;
+
+	public void somar(int valorUm, int valorDois) {
+		this.valor =  this.valor + (valorUm + valorDois);
+		
+	}
+		/*this.valorUm = valorUm;
+		this.valorDois = valorDois;
+		int soma = this.valorUm + this.valorDois;
+		return soma;*/
+	
+	public void substrair() {
+
+	}
+
+	public void dividir() {
+
+	}
+
+	public void multiplicar() {
+
+	}
+	
+	public int getValor (){
+		return valor;
+	}
+	
+	
+
+}
